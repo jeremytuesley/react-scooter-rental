fde4b630526719be24006fbc891ac3d791937584
jeremytuesley <jeremytuesley@gmail.com>
2022-05-04 01:10:30 +1000

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Nav from './components/Nav';
+// import Landing from './components/Landing';
+import { Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing';
+import BikeCatalog from './pages/BikeCatalog';
 
 function App() {
   return (
     <main>
       <Nav />
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/catalog" element={<BikeCatalog />} />
+      </Routes>
     </main>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../assets/nav.scss';
 
 const Nav = () => {
   return (
     <nav>
-      <div className="business-name">Toptal Bike Rentals</div>
+      <Link to="/">
+        <div className="business-name">Toptal Bike Rentals</div>
+      </Link>
       <div className="nav-list-container">
         <ul className="nav-list">
-          <li className="nav-list-item nav-list-catalog">Rentable Bikes</li>
+          <Link to="/catalog">
+            <li className="nav-list-item nav-list-catalog">Rentable Bikes</li>
+          </Link>
           <li className="nav-list-item">Register</li>|
           <li className="nav-list-item">Sign in</li>
           {/* <li className="nav-list-item">Log out</li> */}
