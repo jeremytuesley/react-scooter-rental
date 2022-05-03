b78608df77400520ec9e3eb7720c01ab47167e12
jeremytuesley <jeremytuesley@gmail.com>
2022-05-03 17:37:09 +1000

    bike filter skeleton

---
+++ src/components/BikeFilter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const BikeFilter = () => {
+  return (
+    <div>
+      <h3>When are you looking to rent?</h3>
+      <div className="user-selections-container">
+        <div className="user-selections">
+          <div className="user-selections-title">City</div>
+          <div className="user-selections-sub-title">Where do you need it?</div>
+        </div>
+        <div className="user-selections">
+          <div className="user-selections-title">Check in</div>
+          <div className="user-selections-sub-title">Add dates</div>
+        </div>
+        <div className="user-selections">
+          <div className="user-selections-title">Check out</div>
+          <div className="user-selections-sub-title">Add dates</div>
+        </div>
+        <div className="user-selections">
+          <div className="user-selections-title">Advanced</div>
+          <div className="user-selections-icon">
+            Hamburger, will have model, color, rating selections
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BikeFilter;
---
+++ src/components/BikeList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BikeList = () => {
+  return <div className="bikes-page"></div>;
+};
+
+export default BikeList;
